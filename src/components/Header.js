095154a995b6134f82b7ef8c3174156f00f3cc14
Omran <omranbadawi98@gmail.com>
2021-06-14 23:10:35 +0300

    Add Task

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,30 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = (props) => {
+  const onClick = () => {
+    console.log('Click')
+  }
   return (
-    <header>
+    <header className='header'>
       <h1>{props.title}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.price}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
+      {/* <h1>{props.age}</h1>
+      <h1>{props.price}</h1> */}
     </header>
   )
 }
 
 Header.defaultProps = {
   title: 'Task Tracker',
-  age: 54,
-  price: 300,
+  // age: 54,
+  // price: 300,
 }
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired,
-  price: PropTypes.number.isRequired,
+  // age: PropTypes.number.isRequired,
+  // price: PropTypes.number.isRequired,
 }
 
 export default Header
